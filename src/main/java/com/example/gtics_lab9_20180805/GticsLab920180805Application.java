5be64f7838d6c343be70f0bd8407cf4a0488f323
Daniel <a20180805@pucp.edu.pe>
2023-10-22 21:23:53 -0500

    first commit

---
+++ src/main/java/com/example/gtics_lab9_20180805/GticsLab920180805Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab9_20180805;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab920180805Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab920180805Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gtics_lab9_20180805/GticsLab920180805ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab9_20180805;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab920180805ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
